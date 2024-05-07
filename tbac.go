ad8b8a2ea715e6e8fd330f954ce562b4cfa9c5d1
Sebastian Döll <sebastian@katallaxie.dev>
2024-05-07 13:57:51 +0000

    fix: adding prefix to table name

--- tbac.go
+++ tbac.go
@@ -29,13 +29,13 @@ func RunMigrations(db *gorm.DB) error {
 		return err
 	}
 
-	query := db.Raw("SELECT A.user_id, A.team_id, C.scope as permission FROM user_roles AS A LEFT JOIN role_permissions AS B ON A.role_id = B.role_id LEFT JOIN permissions AS C on B.permission_id = C.id;")
+	query := db.Raw("SELECT A.user_id, A.team_id, C.scope as permission FROM " + db.Config.NamingStrategy.TableName("user_roles") + "AS A LEFT JOIN role_permissions AS B ON A.role_id = B.role_id LEFT JOIN permissions AS C on B.permission_id = C.id;")
 	err = db.Migrator().CreateView("vw_user_team_permissions", gorm.ViewOption{Query: query, Replace: true})
 	if err != nil {
 		return err
 	}
 
-	query = db.Raw("SELECT A.key_id, A.team_id, C.scope as permission FROM api_key_roles AS A LEFT JOIN role_permissions AS B ON A.role_id = B.role_id LEFT JOIN permissions AS C on B.permission_id = C.id;")
+	query = db.Raw("SELECT A.key_id, A.team_id, C.scope as permission FROM " + db.Config.NamingStrategy.TableName("api_key_roles") + " AS A LEFT JOIN role_permissions AS B ON A.role_id = B.role_id LEFT JOIN permissions AS C on B.permission_id = C.id;")
 	err = db.Migrator().CreateView("vw_api_key_team_permissions", gorm.ViewOption{Query: query, Replace: true})
 	if err != nil {
 		return err
